99bfc4e6007195fa523b2647893f38a7b08658df
ljq <ljq17754280229@gmail.com>
2026-06-26 16:54:20 +0800

    状态机解法

---
+++ Q0020_better.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int nums[1000005] = {0}; 
+int main() 
+{
+    int n, a, b;
+    scanf("%d", &n);
+    
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d %d",&a,&b);
+        nums[a-1]++;
+        nums[b]--;
+    }
+    int current =0;
+    for(int j=0;j<n;j++)
+    {
+        current=nums[j]+current;
+        nums[j]=current;
+        printf("%d ",nums[j]);
+    }
+    return 0;
+}
