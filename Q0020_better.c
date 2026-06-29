8b76c9e1b317048aede5715afd4b424bc36abd35
ljq <ljq17754280229@gmail.com>
2026-06-29 16:26:15 +0800

    nothing

--- Q0020_better.c
+++ Q0020_better.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-int nums[1000005] = {0}; 
-int main() 
+int nums[1000005] = {0};
+int main()
 {
     int n, a, b;
     scanf("%d", &n);
-    
-    for(int i=0;i<n;i++)
+
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d %d",&a,&b);
-        nums[a-1]++;
+        scanf("%d %d", &a, &b);
+        nums[a - 1]++;
         nums[b]--;
     }
-    int current =0;
-    for(int j=0;j<n;j++)
+    int current = 0;
+    for (int j = 0; j < n; j++)
     {
-        current=nums[j]+current;
-        nums[j]=current;
-        printf("%d ",nums[j]);
+        current = nums[j] + current;
+        nums[j] = current;
+        printf("%d ", nums[j]);
     }
     return 0;
 }
